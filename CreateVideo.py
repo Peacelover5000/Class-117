2b8c337b59611b90486a1c44065ee676e4db2304
Peace~lover✌️ <96773911+Peacelover5000@users.noreply.github.com>
2023-02-01 16:39:57 +0530

    Add files via upload

---
+++ CreateVideo.py
@@ -0,0 +1,35 @@
+import os
+import cv2
+
+
+path = "/Users/apple/Documents/Python/Class-117/Images"
+
+images = []
+
+
+for file in os.listdir(path):
+    name, ext = os.path.splitext(file)
+
+    if ext in ['.gif', '.png', '.jpg', '.jpeg','.jfif']:
+        file_name = path+"/"+file
+
+        print(file_name)
+               
+        images.append(file_name)
+    
+count = len(images)
+
+frame = cv2.imread(images[0])
+height, width, channels = frame.shape
+size= (width, height)
+
+out = cv2.VideoWriter('Sunset.mp4', cv2.VideoWriter_fourcc(*'DIVX'),5, size)
+
+for i in range(0,count-1):
+
+#for i in range(count-1,0,1):
+    frame = cv2.imread(images[i])
+    out.write(frame)
+
+out.release()
+print("Done")
